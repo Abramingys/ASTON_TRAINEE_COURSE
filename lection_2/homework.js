05143675f87b3484652782ba0d2b83fcb032a6db
Vladimir Abramovich <79985371+Abramingys@users.noreply.github.com>
2024-03-26 11:29:07 +0300

    Update homework.js

--- lection_2/homework.js
+++ lection_2/homework.js
@@ -1,44 +1,68 @@
 //Как исправить "одни пятёрки"?
 
-// var result = [];
-// for (var i = 0; i < 5; i++) {
-//     result[i] = function () {
-//         console.log(i);
-//     };
-// }
-// result[0](); //5
-// result[1](); //5
-// result[2](); //5
-// result[3](); //5
-// result[4](); //5
+var result = [];
+for (let i = 0; i < 5; i++) {
+    result[i] = function () {
+        console.log(i);
+    };
+}
+result[0](); //5
+result[1](); //5
+result[2](); //5
+result[3](); //5
+result[4](); //5
 
 //////////////////////////////////////////////////
 
-// function getGroup() {
-//     let students = [];
-//     let i = 0;
-//     while (i < 10) {
-//         students[i] = function() {
-//             console.log(i);
-//         }
-//         i++
-//     }
-//
-//     return students;
-// }
-//
-// let group = getGroup();
-//
-// group[0](); // 10 как исправить на 0
-// group[5](); // 10                  5
+function getGroup() {
+    let students = [];
+    let i = 0;
+    while (i < 10) {
+        let k = i
+        students[k] = function () {
+            console.log(k);
+        }
+        i++
+    }
+
+    return students;
+}
+
+function getGroup() {
+    let students = [];
+
+    for (let i = 0; i < 10; i++) {
+
+        students[i] = function () {
+            console.log(i);
+        }
+    }
+
+    return students;
+}
+
+let group = getGroup();
+
+group[0](); // 10 как исправить на 0
+group[5](); // 10                  5
 
 //////////////////////////////////////////////////
 
 // Напишите функцию multiply, должна принимать произвольное количество аргументов и возвращать их произведение.
 
+function multiply(a) {
+
+    return function (b) {
+
+        return b ? multiply(a * b) : a;
+
+    }
+
+};
+
 // const result1 = multiply(2)(3)(4)();
 // console.log(result1); // Вывод: 24
-// const result2 = multiply(2)(3)(4)(5();
+// const result2 = multiply(2)(3)(4)(5)();
 // console.log(result2); // Вывод: 120
 
 // const result1 = multiply(2)(3)(4)();
@@ -57,3 +81,18 @@
 //     Текст ошибки: "В getUniqArray был передан невалидный параметр. Аргумент arr
 // должен быть массивом чисел".
 
+
+function getUniqArray(arr) {
+
+
+    if (arr.every((i) => Number.isFinite(i))) {
+
+        return Array.from(new Set(arr));
+
+    } else {
+
+        throw new Error('В getUniqArray был передан невалидный параметр. Аргумент arr  должен быть массивом чисел');
+
+    }
+
+};
